d41f3e0fa889042d506c9f1b53ea83c728235bf8
panntod <munjalindra.pandhu@gmail.com>
2024-10-24 10:09:37 +0700

    feat: Add cookie-parser and implements it into middleware

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: true,
+    origin: "*",
     credentials: true,
     allowedHeaders: "Content-Type, Authorization",
   }),

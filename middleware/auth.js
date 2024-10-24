d6ab78d68bf5950c0d13ed32ffcdfb329ca316de
panntod <munjalindra.pandhu@gmail.com>
2024-10-24 10:11:44 +0700

    chore: Get token from cookies and Improve error messages in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,15 @@ const SECRET_KEY = process.env.SECRET_KEY;
 
 auth = (req, res, next) => {
   let token =
-    req.headers.authorization && req.headers.authorization.split(" ")[1];
+    req.cookies["sentolove/token"] || req.headers?.authorization?.split(" ")[1];
 
   if (!token) {
-    res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ message: "Ooooops! Token tidak ditemukan" });
   } else {
     jwt.verify(token, SECRET_KEY, (error, user) => {
       if (error) {
         res.status(401).json({
-          message: "Invalid token",
+          message: "Ooooops! Token tidak valid",
         });
       } else {
         req.loginUser = user;
